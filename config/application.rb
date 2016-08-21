75225bbebde1e06bc9cb66ffb466ffd0bf1f9b18
ktravers <kate.travers@flatironschool.com>
2016-08-21 16:19:10 -0400

    rename app

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'json'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Wedding
+module BeatrixKiddo
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
