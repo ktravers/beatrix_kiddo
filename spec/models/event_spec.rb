ebc3c7244d8f505173848c32ca48904d014c5e93
ktravers <kate.travers@flatironschool.com>
2016-08-26 00:56:49 -0400

    stub out model specs

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 
 describe Event do
+  it { is_expected.to have_many(:users) }
+
+  it { is_expected.to validate_uniqueness_of(:name) }
+
+  describe '#venue_map_url' do
+    it 'builds google map url for venue' do
+    end
+  end
+
+  describe '#gcal_url' do
+    it 'generates google calendar new event url' do
+    end
+  end
+
+  describe '#to_ics' do
+    it 'generates new iCal event' do
+    end
+  end
 end
