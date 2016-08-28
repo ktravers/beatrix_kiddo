803b51867fc48ad969626bd22a3a96687a2fa89e
ktravers <kate.travers@flatironschool.com>
2016-08-28 17:17:58 -0400

    rm unused cruft

--- db/migrate/20160822050046_rename_user_events_to_rsvps.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserEventsToRsvps < ActiveRecord::Migration
-  def change
-    rename_table :user_events, :rsvps
-  end
-end
--- db/migrate/20160822050151_add_sent_accepted_declined_at_to_rsvps.rb
+++
@@ -1,7 +0,0 @@
-class AddSentAcceptedDeclinedAtToRsvps < ActiveRecord::Migration
-  def change
-    add_column :rsvps, :sent_at, :datetime
-    add_column :rsvps, :accepted_at, :datetime
-    add_column :rsvps, :declined_at, :datetime
-  end
-end
