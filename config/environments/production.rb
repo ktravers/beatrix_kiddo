46b0fef5f6dcf35fcb310c9e494cef8de122907b
ktravers <kate.travers@flatironschool.com>
2016-09-18 22:03:20 -0400

    working out config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,13 +80,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.default_url_options = { :host => 'beatrixkiddo.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => 'utf-8'
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'https://kcandkate.us',
-    user_name:            ENV['MAILER_EMAIL'],
-    password:             ENV['MAILER_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'beatrixkiddo.herokuapp.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['MAILER_EMAIL'],
+    password: ENV['MAILER_PASSWORD']
   }
 end
